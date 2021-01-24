dab5b867ec7a83caed4dfe3568be7f3fe5927fa6
ccr <sis.ccr@gamil.com>
2021-01-24 18:25:59 +0545

    added text color

---
+++ lib/utilities/theme/colors.dart
@@ -0,0 +1,15 @@
+import 'package:chautari/utilities/theme/theme_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class ChautariColors {
+  ThemeMode _mode;
+  ChautariColors() {
+    ThemeController theme = Get.find();
+    _mode = theme.mode;
+  }
+
+  Color blackAndWhitecolor() {
+    return _mode == ThemeMode.dark ? Colors.black87 : Colors.white70;
+  }
+}
