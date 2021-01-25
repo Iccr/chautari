2a15a8f12d5f14db578357cd821fdf99c8e9ea3d
ccr <sis.ccr@gamil.com>
2021-01-25 18:55:31 +0545

    redirection if there is any case for redirection

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
+      title: 'Chautari Basti',
       getPages: ChautariRouters().routers,
       themeMode: ThemeMode.light,
       theme: AppTheme.lightTheme(),
