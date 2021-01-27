9ab1ae76212025750702a830af385d9fc59d7cd1
ccr <sis.ccr@gamil.com>
2021-01-27 16:57:05 +0545

    added flutter form

--- lib/view/add_property/add_property.dart
+++ lib/view/add_property/add_property.dart
@@ -1,13 +1,12 @@
-import 'package:chautari/model/menu_item.dart';
-import 'package:chautari/utilities/theme/colors.dart';
 import 'package:chautari/utilities/theme/padding.dart';
+import 'package:chautari/utilities/theme/text_decoration.dart';
 import 'package:chautari/utilities/theme/text_style.dart';
 import 'package:chautari/view/add_property/add_property_controller.dart';
-import 'package:chautari/widgets/chautari_list.dart';
 import 'package:chautari/widgets/search/search.dart';
 import 'package:chautari/widgets/search/search_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 
 class AddProperty extends StatelessWidget {
   final SearchController search = Get.put(SearchController());
@@ -46,20 +45,10 @@ class AddProperty extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.end,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                "District",
-                style: ChautariTextStyles().listTitle,
-                textAlign: TextAlign.left,
-              ),
-              SizedBox(
-                height: 5,
-              ),
               TextField(
                 focusNode: _myFocusNode,
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  hintText: "Select district",
-                ),
+                decoration: ChautariDecoration()
+                    .outlinedBorderTextField(hintText: "Select District"),
                 style: ChautariTextStyles().listSubtitle,
                 controller: _districtTextController,
                 onTap: () async {
@@ -72,33 +61,19 @@ class AddProperty extends StatelessWidget {
       );
     }
 
-    var listItems = addController.listItems;
-
     return Scaffold(
       appBar: AppBar(
         title: Text("Add"),
-        // actions: [
-        //   IconButton(
-        //       icon: Icon(LineIcons.search),
-        //       onPressed: () {
-        //         _openSearch();
-        //       })
-        // ],
       ),
       body: Container(
-        child: ListView.separated(
-          itemCount: listItems.length,
-          separatorBuilder: (context, index) => ChautariList().getSeperator(),
-          itemBuilder: (context, index) {
-            var item = listItems.elementAt(index);
-            return Container(
-              child: (item.title == "District")
-                  ? districtview()
-                  : Container(
-                      height: 100,
-                    ),
-            );
-          },
+        child: Column(
+          children: [
+            districtview(),
+            RaisedButton(
+              onPressed: () {},
+              child: Text("Submit"),
+            )
+          ],
         ),
       ),
     );
