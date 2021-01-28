56693a447ad7bc7aa2573146ec7542c115f64036
ccr <sis.ccr@gamil.com>
2021-01-28 14:01:55 +0545

    marker placed

--- lib/widgets/map/map.dart
+++ lib/widgets/map/map.dart
@@ -1,29 +1,29 @@
-import 'dart:async';
-
 import 'package:chautari/widgets/map/map_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapView extends StatelessWidget {
-  Completer<GoogleMapController> _controller = Completer();
-
   MapController mapController = Get.put(MapController());
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Map"),
-      ),
-      body: GoogleMap(
-        markers: mapController.marker,
-        myLocationEnabled: true,
-        mapType: MapType.hybrid,
-        initialCameraPosition: mapController.cameraPosition,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
+    return GetX<MapController>(
+      init: MapController(),
+      builder: (c) => Scaffold(
+        appBar: AppBar(
+          title: Text("Map"),
+        ),
+        body: GoogleMap(
+          markers: c.marker,
+          myLocationEnabled: true,
+          mapType: MapType.hybrid,
+          initialCameraPosition: c.cameraPosition,
+          onMapCreated: (GoogleMapController controller) {
+            mapController.setMap(controller);
+          },
+          onTap: (latLng) => c.onTapLocation(latLng),
+        ),
       ),
     );
   }
--- lib/widgets/map/map_controller.dart
+++ lib/widgets/map/map_controller.dart
@@ -7,12 +7,15 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:app_settings/app_settings.dart';
+import 'package:geocoding/geocoding.dart';
 
 class MapController extends GetxController {
-  final _cameraPosition = CameraPosition(
-    target: LatLng(27.7172, 85.3240),
-    zoom: 14.4746,
-  ).obs;
+  GoogleMapController mapController;
+
+  final double zoom = 14.4746;
+  var address = "".obs;
+
+  Rx<CameraPosition> _cameraPosition;
 
   var _marker = HashSet<Marker>().obs;
   var _position = Position().obs;
@@ -21,7 +24,11 @@ class MapController extends GetxController {
 
   Position get position => _position.value;
 
-  HashSet<Marker> get marker => _marker.value;
+  HashSet<Marker> get marker {
+    var marker = _marker.value;
+    print(marker);
+    return marker;
+  }
 
   _showPermissionAlert({String title, String message, String textConfirm}) {
     Get.defaultDialog(
@@ -75,24 +82,59 @@ class MapController extends GetxController {
   Set<Marker> _markers = HashSet<Marker>();
   @override
   void onInit() async {
-    // TODO: implement onInit
     super.onInit();
+    _cameraPosition =
+        CameraPosition(target: LatLng(27.7172, 85.3240), zoom: zoom).obs;
     var position = await _determinePosition();
-    _setMarkers(position);
-
-    // var currentPosition =
+    _setMarker(
+      LatLng(position.latitude, position.longitude),
+    );
   }
 
-  _setMarkers(Position position) {
+  _moveCamera(LatLng latLng) {
+    CameraPosition cameraPosition =
+        CameraPosition(target: latLng, zoom: this.zoom);
+    mapController.moveCamera(
+      CameraUpdate.newCameraPosition(cameraPosition),
+    );
+  }
+
+  _setMarker(LatLng latLng) async {
     var marker = Marker(
       markerId: MarkerId("0"),
-      position: LatLng(
-        position.latitude,
-        position.longitude,
-      ),
+      position: latLng,
     );
     HashSet<Marker> newSet = HashSet<Marker>();
     newSet.add(marker);
     this._marker.value = newSet;
+    _moveCamera(latLng);
+    await _getPlaceFrom(latLng);
+  }
+
+  setMap(GoogleMapController mapController) {
+    this.mapController = mapController;
+  }
+
+  onTapLocation(LatLng latLng) {
+    _setMarker(latLng);
+    _moveCamera(latLng);
+  }
+
+  _getPlaceFrom(LatLng latLng) async {
+    List<Placemark> placemarks =
+        await placemarkFromCoordinates(latLng.latitude, latLng.longitude);
+    print("name:   ${placemarks.first.name}");
+    print("street: ${placemarks.first.street}");
+    print("isoCountryCode: ${placemarks.first.isoCountryCode}");
+    print("country: ${placemarks.first.country}");
+
+    print("administrativeArea: ${placemarks.first.administrativeArea}");
+    print("subAdministrativeArea: ${placemarks.first.subAdministrativeArea}");
+    print("locality: ${placemarks.first.locality}");
+    print("subLocality: ${placemarks.first.subLocality}");
+    print("thoroughfare: ${placemarks.first.thoroughfare}");
+    print("subThoroughfare: ${placemarks.first.subThoroughfare}");
+
+    // this.address.value = placemarks.
   }
 }
