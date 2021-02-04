3dcda2958107579c765efed78d92845ce120db14
ccr <sis.ccr@gamil.com>
2021-02-04 08:09:28 +0545

    top and bottom added in TopDown spacer widget

--- lib/widgets/top_down_space_wrapper.dart
+++ lib/widgets/top_down_space_wrapper.dart
@@ -2,22 +2,20 @@ import 'package:chautari/utilities/theme/padding.dart';
 import 'package:flutter/material.dart';
 
 class TopDownPaddingWrapper extends StatelessWidget {
-  final bool shouldHideTopPadding;
-  final bool shouldHideBottomPadding;
+  double top;
+  double bottom;
   Widget child;
-  TopDownPaddingWrapper(
-      {this.child,
-      this.shouldHideTopPadding = false,
-      this.shouldHideBottomPadding = false});
+  TopDownPaddingWrapper({this.child, this.top, this.bottom}) {
+    this.top = top ?? ChautariPadding.standard;
+    this.bottom = bottom ?? ChautariPadding.small5;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-      if (!shouldHideTopPadding) ...[
-        SizedBox(height: ChautariPadding.standard)
-      ],
+      SizedBox(height: top),
       child,
-      SizedBox(height: ChautariPadding.standard),
+      SizedBox(height: bottom),
     ]);
   }
 }
