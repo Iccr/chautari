599fbfa9bea73491cbca0e82e25b08db605cee06
ccr <sis.ccr@gamil.com>
2021-02-04 23:00:17 +0545

    added on tap in map card

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -1,6 +1,7 @@
 class AppConstant {
   static String recentEmail = "chautari_recent_email";
   static String userKey = "chautari_user";
+  static String roomsKey = "roomsKey";
   static String darktheme = "Dark Theme";
   static String lighttheme = "Light Theme";
   static String themeKey = "Theme";
--- lib/view/explore/explore_controller.dart
+++ lib/view/explore/explore_controller.dart
@@ -1,5 +1,6 @@
 import 'package:chautari/model/rooms_model.dart';
 import 'package:chautari/repository/rooms_repository.dart';
+import 'package:chautari/utilities/constants.dart';
 
 import 'package:get/get.dart';
 
@@ -19,6 +20,7 @@ class ExploreController extends GetxController {
   fetchRooms() async {
     _isLoading.value = true;
     var models = await RoomsRepository().fetchRooms();
+    Get.put(models, tag: AppConstant.roomsKey);
     _isLoading.value = false;
     if (models.errors?.isEmpty ?? false) {
       this.error = models.errors?.first?.value;
