cd5fbd12ac332df7c641455a6acbd6069b2c5775
ccr <sis.ccr@gamil.com>
2021-02-06 18:38:03 +0545

    deleted

--- lib/services/delete_room_service.dart
+++ lib/services/delete_room_service.dart
@@ -1,18 +1,15 @@
 import 'package:chautari/model/room_model.dart';
-
 import 'package:chautari/repository/rooms_repository.dart';
-
 import 'package:get/get.dart';
 
 class DeleteRoomService extends GetxController {
   var _room = RoomModel().obs;
-  var _isLoading = false.obs;
+  var isLoading = false.obs;
   var _error = "".obs;
-  bool get isLoading => _isLoading.value;
+
   String get error => _error.value.isEmpty ? null : _error.value;
 
-  var _success = false.obs;
-  bool get deleteSuccess => this._success.value;
+  var success = false.obs;
 
   DeleteRoomService(RoomModel room) {
     this._room.value = room;
@@ -23,15 +20,15 @@ class DeleteRoomService extends GetxController {
   }
 
   deleteRooms() async {
-    _isLoading.value = true;
+    isLoading.value = true;
     var models = await RoomsRepository().deleteRoom(_room.value.id);
-    _isLoading.value = false;
+    isLoading.value = false;
 
     if (models.errors?.isEmpty ?? false) {
-      this._success.value = false;
+      this.success.value = false;
       this._error.value = models.errors?.first?.value;
     } else {
-      this._success.value = true;
+      this.success.value = true;
     }
   }
 }
