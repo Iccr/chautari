1799edcc1e70c99f4573dac6fe9b10f79c71cf40
ccr <sis.ccr@gamil.com>
2021-02-06 19:04:59 +0545

    deleted room

--- lib/view/room/my_rooms/my_room_controller.dart
+++ lib/view/room/my_rooms/my_room_controller.dart
@@ -1,15 +1,17 @@
 import 'package:chautari/model/room_model.dart';
-import 'package:chautari/repository/rooms_repository.dart';
+import 'package:chautari/services/fetch_my_room_service.dart';
 import 'package:chautari/view/login/auth_controller.dart';
 import 'package:get/get.dart';
 
 class MyRoomsController extends GetxController {
   final AuthController auth = Get.find();
-  bool isLoading = false;
+  FetchMyRoomService myRoomService = Get.put(FetchMyRoomService());
+  var isLoading = false.obs;
   String error;
-  List<RoomModel> models;
+  var models = List<RoomModel>().obs;
 
   get length => models.length;
+
   @override
   onInit() {
     if (auth.user.isLoggedIn) {
@@ -17,15 +19,9 @@ class MyRoomsController extends GetxController {
     }
   }
 
-  _fetchMyRooms() async {
-    isLoading = true;
-    var models = await RoomsRepository().fetchMyRooms();
-    isLoading = false;
-    if (models.errors?.isEmpty ?? false) {
-      this.error = models.errors?.first?.value;
-    } else {
-      this.models = models.rooms;
-    }
-    update();
+  _fetchMyRooms() {
+    isLoading = myRoomService.isLoading;
+    models = myRoomService.rooms;
+    myRoomService.fetchMyRooms();
   }
 }
