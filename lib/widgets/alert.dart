e9461e0f0635b808e37f34d831858bb66352d613
ccr <sis.ccr@gamil.com>
2021-02-07 12:18:27 +0545

    changed alert title

--- lib/widgets/alert.dart
+++ lib/widgets/alert.dart
@@ -5,7 +5,7 @@ import 'package:get/get.dart';
 class Alert extends StatelessWidget {
   static show({
     @required String message,
-    String title = "Alert",
+    String title = "Info",
     String textConfirm = "ok",
     String textCancel = "cancel",
     Function onConfirm,
