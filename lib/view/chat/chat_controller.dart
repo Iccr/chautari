ca75485967fa480b14fb0cdf35e525fc596fa48c
ccr <sis.ccr@gamil.com>
2021-02-12 23:56:24 +0545

    seperated socket

--- lib/view/chat/chat_controller.dart
+++ lib/view/chat/chat_controller.dart
@@ -1,75 +1,14 @@
 import 'package:chautari/model/conversation_model.dart';
 import 'package:chautari/services/new_conversations.dart';
-import 'package:chautari/utilities/api_service.dart';
+import 'package:chautari/utilities/socket.dart';
 import 'package:chautari/view/login/auth_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:keyboard_actions/external/platform_check/platform_check.dart';
-import 'package:phoenix_wings/phoenix_wings.dart';
 
 class ChatViewModel {
   List<Conversation> conversation;
   List<Messages> messages;
   int recipient;
-}
-
-class SocketController extends GetxController {
-  PhoenixSocket _socket;
-  PhoenixChannel _channel;
-  final String token;
-  final int conversationId;
-  SocketController({@required this.token, @required this.conversationId}) {
-    _initConnection(token);
-  }
-  var message = Messages().obs;
-
-  @override
-  onClose() {
-    super.onClose();
-    disconnect();
-  }
-
-  disconnect() {
-    _socket.disconnect();
-  }
-
-  Future<PhoenixSocket> _initConnection(String token) async {
-    _socket = new PhoenixSocket(BaseUrl().socketUrl,
-        socketOptions: PhoenixSocketOptions(params: {"token": token}));
-    await _socket.connect();
-    return _socket;
-  }
-
-  initSocketAndListenChannel() async {
-    _channel = _createChannel()..join();
-
-    _channel?.on("shout", (payload, ref, joinRef) {
-      this.message.value = Messages.fromJson(payload);
-    });
-  }
-
-  String _getRoomName() {
-    return "rent_room:$conversationId";
-  }
-
-  PhoenixChannel _createChannel() {
-    return _socket.channel(_getRoomName(), {});
-  }
-
-  sendMessage(String message, int senderId) {
-    if (message.isNotEmpty) {
-      var _params = {
-        "content": message,
-        "sender_id": senderId,
-        "conversation_id": conversationId
-      };
-
-      _channel?.push(
-        event: "shout",
-        payload: _params,
-      );
-    }
-  }
 }
 
 class ChatController extends GetxController {
@@ -166,7 +105,6 @@ class ChatController extends GetxController {
   }
 
   scrollToLast() async {
-    print("scrolling to last");
     await Future.delayed(Duration(milliseconds: 200));
     scrollController.jumpTo(scrollController.position.maxScrollExtent);
   }
