66538919f214719ede3e6ea974543e13948db1f4
ccr <sis.ccr@gamil.com>
2021-02-16 00:05:30 +0545

    adding services

--- lib/utilities/router/routers.dart
+++ lib/utilities/router/routers.dart
@@ -6,7 +6,6 @@ import 'package:chautari/view/login/login_view.dart';
 import 'package:chautari/view/room/my_rooms/my_room.dart';
 import 'package:chautari/view/room/room_detail/room_detail.dart';
 import 'package:chautari/view/room/room_location_map/room_location_map.dart';
-import 'package:chautari/view/room/room_location_map/show_room_location_map_controller.dart';
 import 'package:chautari/view/room/update_room/update_room.dart';
 import 'package:chautari/view/tab/tab_view.dart';
 import 'package:chautari/widgets/location_picker.dart';
