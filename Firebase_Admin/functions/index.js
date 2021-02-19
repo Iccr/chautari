f33f4d8c1f5ad02e5d54708310a6d299c565d41f
ccr <sis.ccr@gamil.com>
2021-02-19 16:01:13 +0545

    added sending logic

--- Firebase_Admin/functions/index.js
+++ Firebase_Admin/functions/index.js
@@ -1,9 +1,62 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+admin.initializeApp()
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.sendNotification = functions.firestore
+  .document('chats/{chatId}/conversations/{conversationId}/messages')
+  .onCreate((snap, context) => {
+    console.log('----------------start function--------------------')
+
+    const doc = snap.data()
+    console.log(doc)
+
+    const idFrom = doc.idFrom
+    const idTo = doc.idTo
+    const contentMessage = doc.content
+
+    // Get push token user to (receive)
+    admin
+      .firestore()
+      .collection('users')
+      .where('id', '==', idTo)
+      .get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(userTo => {
+          console.log(`Found user to: ${userTo.data().nickname}`)
+          if (userTo.data().fcm && userTo.data().chattingWith !== idFrom) {
+            // Get info user from (sent)
+            admin
+              .firestore()
+              .collection('users')
+              .where('id', '==', idFrom)
+              .get()
+              .then(querySnapshot2 => {
+                querySnapshot2.forEach(userFrom => {
+                  console.log(`Found user from: ${userFrom.data().nickname}`)
+                  const payload = {
+                    notification: {
+                      title: `You have a message from "${userFrom.data().nickname}"`,
+                      body: contentMessage,
+                      badge: '1',
+                      sound: 'default'
+                    }
+                  }
+                  // Let push to the target device
+                  admin
+                    .messaging()
+                    .sendToDevice(userTo.data().fcm, payload)
+                    .then(response => {
+                      console.log('Successfully sent message:', response)
+                    })
+                    .catch(error => {
+                      console.log('Error sending message:', error)
+                    })
+                })
+              })
+          } else {
+            console.log('Can not find pushToken target user')
+          }
+        })
+      })
+    return null
+  })
