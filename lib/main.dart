936261f5519aabdcc4a14a64e19f11aab0a98716
ccr <sis.ccr@gamil.com>
2021-02-25 17:37:38 +0545

    adding analytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,16 @@
+import 'package:chautari/services/analytics_service.dart';
 import 'package:chautari/services/appinfo_service.dart';
-import 'package:chautari/utilities/middleWare.dart';
-import 'package:chautari/utilities/router/router_name.dart';
+
 import 'package:chautari/utilities/router/routers.dart';
 import 'package:chautari/utilities/theme/theme.dart';
 import 'package:chautari/utilities/theme/theme_controller.dart';
 import 'package:chautari/services/room_service.dart';
 import 'package:chautari/view/login/auth_controller.dart';
 import 'package:chautari/view/login/login_view.dart';
-import 'package:chautari/view/map/rooms_map_controller.dart';
+
 import 'package:chautari/view/splash/splash_screen.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:cloud_functions/cloud_functions.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_analytics/observer.dart';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
@@ -50,25 +49,9 @@ void main() async {
 void initServices() async {
   print('starting services ...');
 
-  /// Here is where you put get_storage, hive, shared_pref initialization.
-  /// or moor connection, or whatever that's async.
-  ///
-  // String host = defaultTargetPlatform == TargetPlatform.android
-  //     ? '10.0.2.2:5002'
-  //     : 'localhost:5002';
-
-  //     FirebaseFunctions.instance
-  // .useFunctionsEmulator(origin: 'http://localhost:5000')
-  // .httpsCallable('myCloudFunction');
-
-// Set the host as soon as possible.
   await Firebase.initializeApp();
 
-  // FirebaseFirestore.instance.settings = Settings(host: host, sslEnabled: false);
-  // FirebaseFunctions.instance
-  //     .useFunctionsEmulator(origin: 'http://localhost:5001');
-  // FirebaseAuth.instance.useEmulator('http://localhost:5000');
-
+  await Get.putAsync(() => AnalyticsService().init());
   await Get.putAsync(() => AppInfoService().init());
   await Get.putAsync(() => RoomService().init());
 
@@ -95,7 +78,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    AnalyticsService analyticsService = Get.find();
     return GetMaterialApp(
+      navigatorObservers: [
+        FirebaseAnalyticsObserver(analytics: analyticsService.analytics)
+      ],
       debugShowCheckedModeBanner: false,
       title: 'Chautari Basti',
       routingCallback: (route) {
