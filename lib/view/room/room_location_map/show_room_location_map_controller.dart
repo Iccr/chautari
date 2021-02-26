779a3f450e91c0d77e171f518baa659ab0e67aaf
ccr <sis.ccr@gamil.com>
2021-02-26 07:34:54 +0545

    added markers

--- lib/view/room/room_location_map/show_room_location_map_controller.dart
+++ lib/view/room/room_location_map/show_room_location_map_controller.dart
@@ -1,22 +1,19 @@
 import 'package:chautari/model/room_model.dart';
-import 'package:chautari/widgets/map/map.dart';
 import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class ShowRoomLocationMapController extends GetxController {
-  ChautariMapController mapController;
-
+  GoogleMapController mapController;
   RoomModel room;
-  // RxBool isMapReady;
+
+  var _zoom = 14.4746.obs;
 
   var ready = false.obs;
 
-  var markers = Set<Marker>().obs;
+  var cameraPosition =
+      CameraPosition(target: LatLng(27.7172, 85.3240), zoom: 14).obs;
 
-  ShowRoomLocationMapController() {
-    // mapController = ChautariMapController();
-    // mapController.setZoom(16.0);
-  }
+  var markers = Set<Marker>().obs;
 
   setMarker() {
     LatLng latLng = LatLng(room.lat, room.long);
@@ -24,8 +21,6 @@ class ShowRoomLocationMapController extends GetxController {
     Set<Marker> newSet = Set<Marker>();
     newSet.add(marker);
     this.markers.value = newSet;
-    // moveCamera(latLng);
-    // setLatLong(latLng);
   }
 
   Marker createMarker(LatLng latLng) {
@@ -35,18 +30,20 @@ class ShowRoomLocationMapController extends GetxController {
     );
   }
 
+  moveCamera(LatLng latLng) {
+    CameraPosition cameraPosition =
+        CameraPosition(target: latLng, zoom: this._zoom.value);
+    this.mapController.moveCamera(
+          CameraUpdate.newCameraPosition(cameraPosition),
+        );
+  }
+
   LatLng get roomLatLng => LatLng(room.lat, room.long);
 
   @override
   void onInit() {
     super.onInit();
     room = Get.arguments;
-
-    // try {
-    //   mapController = Get.find();
-    // } catch (e) {
-    //   mapController = Get.put(ChautariMapController());
-    // }}
   }
 
   setMarkers() {
