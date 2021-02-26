f777587dd59a792d70b97bc02fc1c126cdf22f55
ccr <sis.ccr@gamil.com>
2021-02-26 08:30:38 +0545

    added picker

---
+++ lib/widgets/location_picker_controller.dart
@@ -0,0 +1,53 @@
+import 'package:chautari/model/room_model.dart';
+import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class LocationPickerController extends GetxController {
+  GoogleMapController mapController;
+  RoomModel room;
+
+  var _zoom = 14.4746.obs;
+
+  var ready = false.obs;
+
+  var cameraPosition =
+      CameraPosition(target: LatLng(27.7172, 85.3240), zoom: 14).obs;
+
+  var markers = Set<Marker>().obs;
+
+  setMarker(LatLng latLng) {
+    var marker = createMarker(latLng);
+    Set<Marker> newSet = Set<Marker>();
+    newSet.add(marker);
+    this.markers.value = newSet;
+  }
+
+  Marker createMarker(LatLng latLng) {
+    return Marker(
+      markerId: MarkerId("0"),
+      position: latLng,
+    );
+  }
+
+  moveCamera(LatLng latLng) {
+    CameraPosition cameraPosition =
+        CameraPosition(target: latLng, zoom: this._zoom.value);
+    this.mapController.moveCamera(
+          CameraUpdate.newCameraPosition(cameraPosition),
+        );
+  }
+
+  onTapLocation(LatLng latLng) {
+    // LatLng latLng = LatLng(room.lat, room.long);
+    setMarker(latLng);
+    moveCamera(latLng);
+  }
+
+  LatLng get roomLatLng => LatLng(room.lat, room.long);
+
+  @override
+  void onInit() {
+    super.onInit();
+    room = Get.arguments;
+  }
+}
